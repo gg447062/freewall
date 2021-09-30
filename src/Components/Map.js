58de7c47dfae3504ef8d2098263b6b257aeb0ed7
gg447062 <gg447062@gmail.com>
2021-09-29 19:00:13 -0500

    adds grid and map

---
+++ src/Components/Map.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { mapGrid } from '../grid';
+
+export default class Map extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    const { hideMap } = this.props;
+    return (
+      <div id="map-container">
+        {mapGrid.map((square) => (
+          <div className="map-tile" key={square.id}></div>
+        ))}
+        <div onClick={hideMap}>back</div>
+      </div>
+    );
+  }
+}
